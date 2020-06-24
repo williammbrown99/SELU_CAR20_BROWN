bb6e541a095d3602ffd1812ce965113a16579d27
William Brown <william.brown-8@selu.edu>
2020-06-24 18:20:30 -0500

    created model

--- SELU_CAR20_BROWN/CODE/EXP1/fc_1.py
+++ SELU_CAR20_BROWN/CODE/EXP1/fc_1.py
@@ -22,57 +22,12 @@ Distribution: Participants of SELU - CAR System -- LBRN 2020 Virtual Summer Prog
 
 ~~~~'''
 
-#Flattening Images
+'''IMPORTS'''
+#imports
+import tensorflow as tf
 import numpy as np
 
-PATH_VOI = 'SELU_CAR20_BROWN/CODE/INPUT/Voi_Data/'
-
-#VOI Data
-DataSet_xy = 'data_balanced_6Slices_1orMore_xy'
-DataSet_xz = 'data_balanced_6Slices_1orMore_xz'
-DataSet_yz = 'data_balanced_6Slices_1orMore_yz'
-
-#Flattening xy images
-f2 = open(PATH_VOI + DataSet_xy + '.bin','rb') 
-train_set_all_xy = np.load(f2)
-train_label_all_xy = np.load(f2)
-test_set_all_xy = np.load(f2)
-test_label_all_xy = np.load(f2)
-f2.close()
-
-flatImage_xy = []
-for i in range(len(train_label_all_xy)):
-    flatImage_xy.append(train_set_all_xy[i]) 
-
-print(flatImage_xy) #Shape (2520, 3136) (imgNum, imgPixels)
-
-#flattening xz images
-f3 = open(PATH_VOI + DataSet_xz + '.bin','rb') 
-train_set_all_xz = np.load(f3)
-train_label_all_xz = np.load(f3)
-test_set_all_xz = np.load(f3)
-test_label_all_xz = np.load(f3)
-f3.close()
-
-flatImage_xz = []
-for i in range(len(train_label_all_xz)):
-    flatImage_xz.append(train_set_all_xz[i]) 
-
-print(flatImage_xz) #Shape (2520, 3136) (imgNum, imgPixels)
-
-#flattening yz images
-f4 = open(PATH_VOI + DataSet_yz + '.bin','rb') 
-train_set_all_yz = np.load(f4)
-train_label_all_yz = np.load(f4)
-test_set_all_yz = np.load(f4)
-test_label_all_yz = np.load(f4)
-f4.close()
-
-flatImage_yz = []
-for i in range(len(train_label_all_yz)):
-    flatImage_yz.append(train_set_all_yz[i]) 
-
-print(flatImage_yz) #Shape (2520, 3136) (imgNum, imgPixels)
+from tensorflow import keras
 
 '''~~~~ CLASSES ~~~~'''
 #Your class definitions
@@ -88,7 +43,22 @@ print(flatImage_yz) #Shape (2520, 3136) (imgNum, imgPixels)
 
 
 '''~~~~ PARAMETERS ~~~~'''
+###
 #Use tuple simple or multi-dimensional-like for more detail settings. Add aditional constants for detail of each parameters to export.
+PATH_VOI = 'SELU_CAR20_BROWN/CODE/INPUT/Voi_Data/'
+
+#VOI Data
+DataSet_xy = 'data_balanced_6Slices_1orMore_xy'
+DataSet_xz = 'data_balanced_6Slices_1orMore_xz'
+DataSet_yz = 'data_balanced_6Slices_1orMore_yz'
+
+#Obtaining data
+f2 = open(PATH_VOI + DataSet_xy + '.bin','rb') 
+train_set_all_xy = np.load(f2)          #training parameter
+train_label_all_xy = np.load(f2)        #training labels
+test_set_all_xy = np.load(f2)
+test_label_all_xy = np.load(f2)
+f2.close()
 
 #Experiment ID
 EXP_ID = 'Exp_1'
@@ -99,7 +69,8 @@ EXP_ID = 'Exp_1'
 NUM_NODE = (1,1)
 
 #Model Parameters
-#ACT_FUN = ('sigmoid', 'selu', 'relu'): The activation functions in the first and the second hidden layers are Sigmoid and SeLU, respectively. At the output layer, it is ReLU.
+#ACT_FUN = ('sigmoid', 'selu', 'relu'): The activation functions in the first and the second hidden layers are Sigmoid and SeLU,
+# respectively. At the output layer, it is ReLU.
 
 ACT_FUN = ('relu') #Activation function.
 INITZR = ('RandomNormal') #Initializer
@@ -112,7 +83,21 @@ OPTMZR = ('SGD')
 
 '''~~~~ MODEL SETUP ~~~~'''
 #your code to create the model
+initializer = tf.keras.initializers.RandomNormal(mean=0., stddev=1.)    #initializer, random normal 
+values = initializer(shape=(2, 2))
 
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(56, 56)), #flattening images
+    keras.layers.Dense(5, activation='sigmoid'),#hidden layer, sigmoid activation function
+    keras.layers.Dense(5, activation='selu'),   #second hidden layer, selu activation function
+    keras.layers.Dense(2, activation='relu')    #output layer, relu activation function
+])
+
+model.compile(optimizer='SGD',                  #optimizer, Stochastic gradient descent
+              loss='mean_squared_error',        #loss function, mean squared error
+              metrics=['accuracy'])             
+
+model.fit(train_set_all_xy, train_label_all_xy, epochs=10)  #training model
 
 '''~~~~ LOAD DATA ~~~~'''
 #your code to load data. Export ID of each sample as 'train', 'val', 'test' as a csv and binary file sampleID.csv and sampleID.bin
