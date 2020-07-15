055650738fdc8e6eab30a880e2616ab13a9c1902
William Brown <william.brown-8@selu.edu>
2020-07-15 02:43:42 -0500

    Created Algorithm

--- CadLung/EXP1/NasFc.py
+++ CadLung/EXP1/NasFc.py
@@ -3,7 +3,7 @@ SELU - CAR System -- LBRN 2020 Virtual Summer Program
 Mentor: Dr. Omer Muhammet Soysal
 Last Modified on: July 9 2020
 
-Authors:
+Authors: William Brown
 Date:
 Distribution: Participants of SELU - CAR System -- LBRN 2020 Virtual Summer Program
 
@@ -21,17 +21,21 @@ HELP
 #import os
 #import keras as ks
 #from keras import backend as k
+import keras
 
 from keras import callbacks as cB
-from keras import layers as kLyr
 from keras import models as kModels
-from keras.layers import Activation, Dense, Flatten
+from keras.layers import Activation, Dense, Flatten, Input
 from keras.metrics import AUC, FalsePositives, TrueNegatives, Precision, Recall
 from keras.models import Sequential
 from scipy import stats
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+from keras.models import Model
 
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 
 '''~~~~ CLASSES ~~~~'''
 
@@ -42,10 +46,8 @@ import numpy as np
 def rangeNormalize(data, lower, upper): #lower, upper = range
     """function to range normalize data"""
     scaler = MinMaxScaler(feature_range=(lower, upper))
-    nsamples, nx, ny = data.shape
-    twoDimData = data.reshape((nsamples, nx*ny)) #reshaping 3d to 2d
-    normalized = scaler.fit_transform(twoDimData)
-    return normalized.reshape(nsamples, nx, ny) #reshaping back to 3d
+    normalized = scaler.fit_transform(data)
+    return normalized
 #####
 
 '''~~~~ PARAMETERS ~~~~'''
@@ -54,7 +56,7 @@ PATH_VOI = 'CadLung/INPUT/Voi_Data/'
 PATH_EXP = 'CadLung/EXP1/'
 
 #Data
-NORMALIZE = 'none' #Options: {Default: 'none', 'range', 'z-score'}
+NORMALIZE = 'z-score' #Options: {Default: 'none', 'range', 'z-score'}
 
 #Model
 VAL_SPLIT = 0.15
@@ -63,17 +65,17 @@ BATCH_SIZE = 32
 METRICS=['accuracy', AUC(), Recall(), Precision(), FalsePositives(), TrueNegatives()]
 LOSS_F = 'mean_squared_error'
 OPT_M = 'SGD'
-checkpoint_filepath = PATH_EXP+'CHKPNT/'
+checkpoint_filepath = PATH_EXP+'CHKPNT/bestWeights.hdf5'
 modelChkPnt_cBk = cB.ModelCheckpoint(filepath=checkpoint_filepath,
                                                save_weights_only=True,
-                                               monitor='val_acc',
+                                               monitor='val_accuracy',
                                                mode='max',
                                                save_best_only=True)
 clBacks = [modelChkPnt_cBk]
 
 #Network Architecture
-MAX_NUM_NODES = (None,6,6,6,1)   #first layer, hidden layers, and output layer. #hidden notes > 1.
-lenMaxNumHidenLayer = len(MAX_NUM_NODES) - 2
+MAX_NUM_NODES = (None,6,6,6,1)   #first layer, hidden layers, and output layer. #hidden nodes > 1.
+lenMaxNumHidenLayer = len(MAX_NUM_NODES) - 2    #3
 ACT_FUN = (None,'sigmoid','sigmoid','sigmoid','relu')
 
 #Export Parameters into parameter.tf
@@ -100,13 +102,6 @@ with open(PATH_VOI + DataSet_xy + '.bin','rb') as f2:
 
 
 '''~~~~ PRE-PROCESS ~~~~'''
-#your code to pre-proces data. Export pre-processed data if takes too long to create as a binary file dataPreProcess.bin
-#Dim1: Batch, (Dim2,Dim3): Flattened images
-train_set_all_xy = np.reshape(train_set_all_xy, (train_set_all_xy.shape[0], 1, train_set_all_xy.shape[1]))
-#Dim1: Batch, (Dim2,Dim3): Flattened images
-test_set_all_xy = np.reshape(test_set_all_xy, (test_set_all_xy.shape[0], 1, test_set_all_xy.shape[1]))
-#train_label_all_xy = ks.utils.to_categorical(train_label_all_xy)
-print(train_set_all_xy.shape)
 #Normalize your data 
 if NORMALIZE[0] == 'r':
     train_set_all_xy = rangeNormalize(train_set_all_xy, 0, 1)
@@ -115,11 +110,15 @@ elif NORMALIZE[0] == 'z':
     train_set_all_xy = stats.zscore(train_set_all_xy)
     test_set_all_xy = stats.zscore(test_set_all_xy)
 #
-
+#your code to pre-proces data. Export pre-processed data if takes too long to create as a binary file dataPreProcess.bin
+#Dim1: Batch, (Dim2,Dim3): Flattened images
+train_set_all_xy = np.reshape(train_set_all_xy, (train_set_all_xy.shape[0], 1, train_set_all_xy.shape[1]))
+#Dim1: Batch, (Dim2,Dim3): Flattened images
+test_set_all_xy = np.reshape(test_set_all_xy, (test_set_all_xy.shape[0], 1, test_set_all_xy.shape[1]))
+#train_label_all_xy = keras.utils.to_categorical(train_label_all_xy)
 
 #Export pre-processed into data dataPreProcess.tf if takes too long to create 
 ''' COMPLETE THE CODE '''
-
 
 
 '''~~~~ MODEL SETUP & TRAINING  ~~~~'''
@@ -127,41 +126,45 @@ elif NORMALIZE[0] == 'z':
 bestAcc = 0 #best accuracy score
 
 bestModel = keras.Sequential([
-    kLyr.Flatten(),
-    kLyr.Dense(1, activation=ACT_FUN[1], kernel_initializer='random_normal'), #1st hidden layer
-    kLyr.Dense(1, activation=ACT_FUN[-1], kernel_initializer='random_normal') #output layer
+    Flatten(),
+    Dense(1, activation=ACT_FUN[1], kernel_initializer='random_normal'), #1st hidden layer
+    Dense(1, activation=ACT_FUN[-1], kernel_initializer='random_normal') #output layer
 ])
 
 bestModel.compile(loss=LOSS_F, optimizer=OPT_M, metrics=METRICS)
 
-modelFit = bestModel.fit(train_set_all_xy, train_label_all_xy, epochs=EPOCHS,verbose=0,validation_split = VAL_SPLIT)
-numNodeLastHidden = np.zeros(lenMaxNumHidenLayer + 1) #1st one is for input. number of nodes added to the current layer
-#Searching the best network architecture
-for hL in range(1,lenMaxNumHidenLayer+1):  #Hidden Layer Loop
-    for j in range(2,MAX_NUM_NODES[hL]):   #Node loop
-        numNodeLastHidden[hL] += 1  #A new node added to the current layer
-        #Re-create the temp model with a new node at the layer
-        ''' !!!!!! THIS SECTION NEEDS TO BE COMPLETED   !!!!!! 
-        modelTmp = ..... #initialize temporary model
-        ....
+#modelFit = bestModel.fit(train_set_all_xy, train_label_all_xy, epochs=EPOCHS,verbose=0,validation_split = VAL_SPLIT)
 
-        modelTmp.compile(loss=LOSS_F, optimizer=OPT_M, metrics=METRICS)
-        modelFitTmp = modelTmp.fit(train_data_xy, train_data_label_xy, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=0, callbacks = clBacks, validation_split = VAL_SPLIT)
+numNodeLastHidden = np.zeros(lenMaxNumHidenLayer + 1) #[0 0 0 0] 1st one is for input. number of nodes added to the current layer
+
+#Searching the best network architecture
+for hL in range(1,lenMaxNumHidenLayer+1):  #Hidden Layer Loop (1 to 4)
+    for j in range(2,MAX_NUM_NODES[hL]):   #Node loop   (2 to 6), 3 times
+        numNodeLastHidden[hL] += 1  #A new node added to the current layer [0 0 0]
+        #Re-create the temp model with a new node at the layer
+        modelTmp = keras.Sequential()   #initialize temporary model
+        modelTmp.add(Flatten())         #Input layer
+
+        for i in range(hL):             #Adds number of hidden layers
+            modelTmp.add(Dense(int(numNodeLastHidden[hL]), activation=ACT_FUN[hL], \
+                           kernel_initializer='random_normal'))  #2 nodes, only recieves input from input layer
+        
+        #output layer
+        modelTmp.add(Dense(1, activation=ACT_FUN[-1], kernel_initializer='random_normal'))
+
+
+        modelTmp.compile(loss=LOSS_F, optimizer=OPT_M, metrics=['accuracy']) #, metrics=METRICS)
+        modelFitTmp = modelTmp.fit(train_set_all_xy, train_label_all_xy, batch_size=BATCH_SIZE,\
+                                   epochs=EPOCHS, verbose=0, callbacks = clBacks,\
+                                   validation_split=VAL_SPLIT)
 
         #Get the best weights
-        ''' #!!!!!! THIS SECTION NEEDS TO BE COMPLETED   !!!!!! 
+        #!!!!!! THIS SECTION NEEDS TO BE COMPLETED   !!!!!! 
         #Write the code to get the best set of weights "modelFitTmp" out of all epochs
-        '''
 
-
-
-        #
-
-        ''' #!!!!!! THIS SECTION NEEDS TO BE MODIFIED !!!!!!   
         #After pulling out the best weights and the corresponding model "modelFitTmp", compare against the last "bestAcc"
-        '''
-        if modelFitTmp.history['val_accuracy'] >= bestAcc:   #update the best model and continue adding a node to this layer
-            bestAcc = modelFit.history['val_accuracy']
+        if max(modelFitTmp.history['val_accuracy']) >= bestAcc:   #update the best model and continue adding a node to this layer
+            bestAcc = max(modelFitTmp.history['val_accuracy'])
             bestModel = modelTmp
             del modelTmp    #WHY ?
         else:   #adding a new node did not improve the performance. Stop adding a new node to this layer
@@ -177,8 +180,15 @@ for hL in range(1,lenMaxNumHidenLayer+1):  #Hidden Layer Loop
 
 '''#~~~~ TESTING ~~~~'''
 ''' COMPLETE THE CODE '''
+bestModel.load_weights(checkpoint_filepath)
 
+test_pred = []
+for i in bestModel.predict(test_set_all_xy).reshape(test_label_all_xy.shape[0]):  #reshape from (1092, 1) to (1092)
+    test_pred.append(round(i))
+testAcc = accuracy_score(test_label_all_xy, test_pred)
 
+print('Test Accuracy: {}'.format(testAcc))
+print(bestModel.summary())
 
 
 #Export predictions with sample-IDs into testOutput.tf
